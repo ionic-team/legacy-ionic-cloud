82155ab66ae19b608efb81e94d59c49fbfad906a
Daniel Imhoff <dwieeb@gmail.com>
2016-06-01 13:16:55 -0500

    Test for api_key in reverted code

--- spec/core/app.spec.js
+++ spec/core/app.spec.js
@@ -3,18 +3,19 @@ var App = require("../../dist/es5/core/app").App;
 describe("app", function() {
 
   it("should instantiate", function() {
-    var app = new App("1234abc");
+    var app = new App("1234abc", "stupid_api_key");
     expect(app.id).toBe("1234abc");
+    expect(app.apiKey).toBe("stupid_api_key");
   });
 
   it("should accept gcmKey", function() {
-    var app = new App("1234abc");
+    var app = new App("1234abc", "stupid_api_key");
     app.gcmKey = "something";
     expect(app.gcmKey).toBe("something");
   });
 
   it("should accept devPush", function() {
-    var app = new App("1234abc");
+    var app = new App("1234abc", "stupid_api_key");
     app.devPush = true;
     expect(app.devPush).toBe(true);
   });
