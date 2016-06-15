b2c8c4afd3ba4bc663a2186604d127fe2b3215ab
Daniel Imhoff <dwieeb@gmail.com>
2016-06-15 14:26:25 -0500

    Consolidate angular 1 integration into a single file

--- src/auth/angular.js
+++
@@ -1,9 +0,0 @@
-// Add Angular integrations if Angular is available
-
-if (typeof angular === 'object' && angular.module) {
-  angular.module('ionic.cloud.auth', [])
-
-  .factory('$ionicAuth', [function() {
-    return Ionic.Auth;
-  }]);
-}
