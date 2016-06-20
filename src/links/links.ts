c3ec67ea2605777f349546b4369d1e1b58b3ba30
Max Lynch <max@drifty.com>
2016-06-20 17:47:34 -0500

    Links

--- src/links/links.ts
+++ src/links/links.ts
@@ -1,36 +1,37 @@
-import { Client } from '../core/client';
 import { IonicCloud } from '../core/core';
 
 import { DeferredPromise } from '../core/promise';
 
 export class Links {
-
-  constructor(public client: Client, public appId: string) {
-    this.client = client;
-    this.appId = appId;
+  constructor() {
+    this.init();
   }
 
-  static _post(url, data) {
+  init() {
+    this.checkInstall({}).then((resp) => {
+    }, (err) => {
+    });
+  }
+
+  _post(url, data) {
     var q = new DeferredPromise<any>();
 
     let appId = IonicCloud.config.get('app_id');
     data.app_id = appId;
 
-    let client = IonicCloud.client;
-
-    client.post(url)
+    IonicCloud.client.post(url)
       .send(data)
       .end((err, res) => {
         if (err) {
           q.reject({
             error: err,
             response: res,
-            data: (res.body && res.body.error) || {}
+            data: (res && res.body && res.body.error) || {}
           });
         } else {
           q.resolve({
             response: res,
-            data: (res.body && res.body.data) || {}
+            data: (res && res.body && res.body.data) || {}
           });
         }
       });
@@ -38,11 +39,15 @@ export class Links {
     return q.promise;
   }
 
-  static link(data): Promise<any> {
-    return Links._post('/links/link', data);
+  checkInstall(data): Promise<any> {
+    return this._post('/links/install', data);
   }
 
-  static content(data): Promise<any> {
-    return Links._post('/links/content', data);
+  link(data): Promise<any> {
+    return this._post('/links/link', data);
+  }
+
+  content(data): Promise<any> {
+    return this._post('/links/content', data);
   }
 }
