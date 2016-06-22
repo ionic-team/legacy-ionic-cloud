405ccad31768d9fab4f20d30f2cb8cbf06b6c642
Max Lynch <max@drifty.com>
2016-06-22 15:40:25 -0500

    Links

--- src/links/links.ts
+++ src/links/links.ts
@@ -1,6 +1,11 @@
 import { IonicCloud } from '../core/core';
 
+import { deepExtend } from '../util/util';
+
 import { DeferredPromise } from '../core/promise';
+
+declare var navigator;
+declare var window;
 
 export class Links {
   constructor() {
@@ -8,7 +13,7 @@ export class Links {
   }
 
   init() {
-    this.checkInstall({}).then((resp) => {
+    this.checkInstall().then((resp) => {
     }, (err) => {
     });
   }
@@ -39,8 +44,48 @@ export class Links {
     return q.promise;
   }
 
-  checkInstall(data): Promise<any> {
-    return this._post('/links/install', data);
+  _getHardwareInfo() {
+    var w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0];
+
+    var info = {
+      x: w.innerWidth || e.clientWidth || g.clientWidth,
+      y: w.innerHeight || e.clientHeight || g.clientHeight,
+      tzOffset: new Date().getTimezoneOffset(),
+      cpuClass: navigator.cpuClass,
+      platform: navigator.platform,
+      doNotTrack: navigator.doNotTrack
+    };
+
+    return info;
+  }
+
+  checkInstall(): Promise<any> {
+    let data = this._getHardwareInfo();
+
+    if (window.IonicDeeplink) {
+      return new Promise((resolve, reject) => {
+        window.IonicDeeplink.getHardwareInfo((info) => {
+          console.log('Got native hardware info', info);
+          let newData = deepExtend(data, info);
+
+          this._post('/links/install', {
+            data: newData
+          }).then((resp) => {
+            resolve(resp);
+          }, (err) => {
+            reject(err);
+          });
+        });
+      });
+    } else {
+      console.warn('Ionic Links: ionic-plugin-deeplinks is not installed, device fingerprinting will be inaccurate.');
+      return this._post('/links/install', {
+        data: data
+      });
+    }
   }
 
   link(data): Promise<any> {
