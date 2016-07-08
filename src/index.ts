e6d75947bfbafe2aa005759a1b84000cdec263f6
Nicholas Hyatt <nhyatt@wisc.edu>
2016-07-08 15:55:17 -0500

    updating branch from master changes

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ export { Cordova } from './cordova';
 export { Core } from './core';
 export { Deploy } from './deploy/deploy';
 export { Device } from './device';
+export { Database, DBSettings } from './database/database';
 export { Container as DIContainer } from './di';
 export { EventEmitter } from './events';
 export { Insights } from './insights';
